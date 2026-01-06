cfbf34f27bd2be836fd8f6bf97109f2b40dced8c
Lordwish Sylvaince <lordwishsylvaince1236@gmail.com>
2026-01-06 23:52:38 +0800

    ..

--- src/analyzer.c
+++ src/analyzer.c
@@ -38,9 +38,12 @@ int analyze_file_stats(const char *path, Stats *out_stats)
 }
 int build_word_freq(const char *path)
 {
+
+}
+void print_top_words(int top_n)
+{
     
 }
-void print_top_words(int top_n);
 int count_word_in_file(const char *path, const char *query);
 
 void free_word_freq(void);
